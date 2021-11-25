cca5af20c30d96e5ebdeb18b682af130a289007a
pacomarm <francisco.marmolejoml@udlap.mx>
2021-11-24 22:48:26 -0600

    working encryption

--- src/App.js
+++ src/App.js
@@ -3,16 +3,33 @@ import openSocket from 'socket.io-client';
 
 import { constants } from './constants/constants';
 import { Messages } from "./pages/msg/Messages";
+import { useState } from "react";
 
 export const  App = () => {
-
+  const [show, setShow] = useState(false)
   const socket = openSocket(constants.socketURL, {transports: ['websocket']});
 
   return (
     <>
       <h1>Hi Chat Page</h1>
-      <Messages socket={socket}/>
-      <ChatPage socket={socket}/>
+      {
+        !show && (
+          <>
+            <form id="form" action="">
+                <input value={message} name="message" autoComplete="off" onChange={handleInputChange}/>
+                <button onClick={sendMessage}>Send</button>
+            </form>
+          </>
+        )
+      }
+      {
+        show && (
+          <>
+            <Messages socket={socket}/>
+            <ChatPage socket={socket}/>
+          </>
+        )
+      }
     </>
   );
 }
